dc9692eba04f45a19bb01ee8519535e19da4de3e
Nathan Kelley <kel18018@byui.edu>
2023-05-24 23:30:05 -0600

    fixed swagger route

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swagger.js');
-router.use('/api-docs', swaggerUi.serve);
-router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+const swaggerDocument = require('../swagger.json');
+router
+    
+  .use((req, res, next) => {
+    res.removeHeader('Content-Type');
+    next();
+  })
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
