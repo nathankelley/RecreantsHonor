b391ee3f24f3cfe90d1306d012b2e0354cf1b195
Nathan Kelley <kel18018@byui.edu>
2023-05-28 00:03:21 -0600

    added https to render config

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: "Recreant's Honor API"
   },
   host: 'recreants-honor-api.onrender.com',
-  schemes: ['http']
+  schemes: ['https']
 };
 
 const outputFile = './swagger.json';
